d7299403cd7be18c232cdb8836d98455651e4674
israt567 <156815648+israt567@users.noreply.github.com>
2024-09-23 21:19:48 +0600

    Add files via upload
    authentication uploaded

---
+++ messManagementSystemFrontEnd/src/Pages/Authentication/Login/Login.jsx
@@ -0,0 +1,93 @@
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { login } from "../../../features/auth/authSlice";
+import { useEffect } from "react";
+
+const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // const user = useSelector((state)=>state.auth);
+
+    const onSubmit = async (data) => {
+        try {
+            const resultAction = await dispatch(login(data));
+
+            if (login.fulfilled.match(resultAction)) {
+                
+                navigate('/user-dashboard');
+            }
+            else {
+                if (resultAction.payload) {
+                    toast.error(resultAction.payload);
+                } else {
+                    toast.error("Login failed");
+                }
+            }
+        } catch (err) {
+            toast.error("Server error");
+        }
+    };
+
+    // if(user.isAuthenticated){
+    //     Navigate('/user-dashboard')
+    // }
+
+    return (
+        <div className="flex items-center justify-center md:my-10">
+            <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="email@example.com"
+                                className="input input-bordered"
+                                {...register("email", { required: "Email is required" })}
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="password"
+                                className="input input-bordered"
+                                {...register("password", { required: "Password is required" })}
+                            />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button type="submit" className="btn btn-primary">
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                    <div className="text-center mt-4">
+                        <p>
+                            Do not Have an account?{" "}
+                            <Link className="text-blue-500" to="/register">
+                                Register
+                            </Link>
+                        </p>
+                    </div>
+                    {/* <div className="text-center mt-4">
+                        <a href="#" className="link link-hover">
+                            Forgot password?
+                        </a>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ messManagementSystemFrontEnd/src/Pages/Profile/Profile.jsx
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faUser, faPhone, faIdCard } from '@fortawesome/free-solid-svg-icons';
+import { changePassword } from '../../features/auth/authSlice';
+import profileImg from "../../assets/profileImg.png"
+
+const ProfilePage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const dispatch = useDispatch();
+
+    const user = useSelector((state) => state.auth.user);
+    const mess = useSelector((state) => state.mess)
+
+    const onSubmit = data => {
+        dispatch(changePassword({
+            userId: user._id,
+            oldPassword: data.oldPassword,
+            newPassword: data.newPassword
+        }))
+            .unwrap()
+            .then((response) => {
+                console.log(response);
+                toast.success(response);
+            })
+            .catch((error) => {
+                toast.error(error);
+            });
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+            <div className="card w-full max-w-2xl shadow-xl bg-white">
+                <div className="card-body">
+                    <div className="flex justify-center mb-4">
+                        <div className="avatar">
+                            <div className="w-24 rounded-full">
+                                {/* <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" /> */}
+                                <img src={profileImg} />
+                            </div>
+                        </div>
+                    </div>
+                    <h2 className="text-center text-2xl font-bold mb-4">Profile</h2>
+                    <div className="my-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faUser} className="mr-3" />
+                            <span>{user?.fullName}</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faEnvelope} className="mr-3" />
+                            <span>{user?.email}</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faPhone} className="mr-3" />
+                            <span>{user?.phoneNumber}</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faIdCard} className="mr-3" />
+                            <span>{user?.nationalId}</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faUser} className="mr-3" />
+                            <span>{user?.role}</span>
+                        </div>
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faUser} className="mr-3" />
+                            <span>{mess?.mess?.messName || "Not Assigned"}</span>
+                        </div>
+                    </div>
+                    <h3 className="text-xl font-bold mt-6 mb-4">Change Password</h3>
+                    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Old Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                {...register("oldPassword", { required: true })}
+                                className={`input input-bordered ${errors.oldPassword ? 'input-error' : ''}`}
+                            />
+                            {errors.oldPassword && <span className="text-error">This field is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                {...register("newPassword", { required: true })}
+                                className={`input input-bordered ${errors.newPassword ? 'input-error' : ''}`}
+                            />
+                            {errors.newPassword && <span className="text-error">This field is required</span>}
+                        </div>
+                        <div className="form-control mt-6 md:col-span-2">
+                            <button type="submit" className="btn btn-primary w-full" >Update Password</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProfilePage;
